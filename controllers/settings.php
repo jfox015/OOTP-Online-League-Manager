98c9138cd157b7c01075e99c90c9a10c643e53ba
Jeff Fox <jfox015@gmail.com>
2012-02-21 01:00:53 -0500

    Moving Settings to DB

--- controllers/settings.php
+++ controllers/settings.php
@@ -10,7 +10,11 @@ class Settings extends Admin_Controller {
 		
 		$this->auth->restrict('Site.Settings.View');
 		//$this->auth->restrict('OOTP.Manager.View');
-		
+
+        if (!class_exists('Activity_model'))
+        {
+            $this->load->model('activities/Activity_model', 'activity_model', true);
+        }
 		$this->lang->load('manager');
 
 	}
@@ -24,81 +28,106 @@ class Settings extends Admin_Controller {
 			$this->$method();
 		}
 	}
-	
-	//--------------------------------------------------------------------
-	public function index()
-	{
-		$this->load->helper('datalist');
+    //--------------------------------------------------------------------
+
+    public function index()
+    {
+        if ($this->input->post('submit'))
+        {
+            if ($this->save_settings())
+            {
+                Template::set_message('League settings successfully saved.', 'success');
+                redirect(SITE_AREA .'/settings/league_manager');
+            } else
+            {
+                Template::set_message('There was an error saving your settings.', 'error');
+            }
+        }
+        // Read our current settings
+        $this->load->helper('datalist');
+        $settings = $this->settings_lib->find_all();
+        Template::set('settings', $settings);
+
+        Template::set('toolbar_title', lang('dbrd_settings_title'));
+        Template::set_view('league_manager/settings/index');
+        Template::render();
+    }
+
+    //--------------------------------------------------------------------
+
+    //--------------------------------------------------------------------
+    // !PRIVATE METHODS
+    //--------------------------------------------------------------------
+
+    private function save_settings()
+    {
+
 		$this->load->library('form_validation');
 
-		if ($this->input->post('submit'))
-		{
-            $this->form_validation->set_rules('ootp_version', lang('dbrd_settings_gamever'), 'required|trim|xss_clean');
-            $this->form_validation->set_rules('league_id', lang('dbrd_settings_leagueid'), 'required|number|trim|xss_clean');
-			$this->form_validation->set_rules('league_name', lang('dbrd_settings_lgname'), 'trim|xss_clean');
-            $this->form_validation->set_rules('league_abbr', lang('dbrd_settings_lgabbr'), 'trim|xss_clean');
-			$this->form_validation->set_rules('league_icon', lang('dbrd_settings_lgicon'), 'trim|xss_clean');
-			$this->form_validation->set_rules('league_txtcolor', lang('dbrd_settings_txtcolor'), 'trim|xss_clean');
-			$this->form_validation->set_rules('league_bgcolor', lang('dbrd_settings_bgcolor'), 'trim|xss_clean');
-			
-			$this->form_validation->set_rules('league_file_path', lang('dbrd_settings_lgdfile'), 'number|xss_clean');
-			$this->form_validation->set_rules('asset_path', lang('dbrd_settings_assetpath'), 'required|trim|xss_clean');
-			$this->form_validation->set_rules('asset_url', lang('dbrd_settings_asseturl'), 'required|trim|xss_clean');
-			
-			$this->form_validation->set_rules('header_img', lang('home_settings_header'), 'trim|xss_clean');
-            $this->form_validation->set_rules('twitter_string', lang('home_settings_twitter'), 'trim|xss_clean');
-            $this->form_validation->set_rules('tweet_count', lang('home_settings_tweets'), 'trim|xss_clean');
-           
-			$this->form_validation->set_rules('sql_path', lang('sql_settings_mysqlpath'), 'required|trim|xss_clean');
-            $this->form_validation->set_rules('max_sql_size', lang('sql_settings_max'), 'number|xss_clean');
-            $this->form_validation->set_rules('auto_split', lang('sql_settings_autosplit'), 'number|xss_clean');
-            $this->form_validation->set_rules('limit_load', lang('sql_settings_auto_load'), 'required|number|xss_clean');
+        $this->form_validation->set_rules('game_version', lang('dbrd_settings_gamever'), 'required|trim|xss_clean');
+        $this->form_validation->set_rules('league_id', lang('dbrd_settings_leagueid'), 'required|number|trim|xss_clean');
+        $this->form_validation->set_rules('league_name', lang('dbrd_settings_lgname'), 'trim|xss_clean');
+        $this->form_validation->set_rules('league_abbr', lang('dbrd_settings_lgabbr'), 'trim|xss_clean');
+        $this->form_validation->set_rules('league_icon', lang('dbrd_settings_lgicon'), 'trim|xss_clean');
+        $this->form_validation->set_rules('league_txtcolor', lang('dbrd_settings_txtcolor'), 'trim|xss_clean');
+        $this->form_validation->set_rules('league_bgcolor', lang('dbrd_settings_bgcolor'), 'trim|xss_clean');
 
-			if ($this->form_validation->run() !== FALSE)
-			{
-				$data = array(
-					'ootp_version'			=> $this->input->post('ootp_version'),
-					'league_id'				=> $this->input->post('league_id'),
-					'use_ootp_details'		=> ($this->input->post('use_ootp_details')) ? 1 : -1,
-					'league_name'			=> $this->input->post('league_name'),
-					'league_abbr'			=> $this->input->post('league_abbr'),
-					'league_icon'			=> $this->input->post('league_icon'),
-					'league_txtcolor'		=> $this->input->post('league_txtcolor'),
-					'league_bgcolor'		=> $this->input->post('league_bgcolor'),
-					
-					'league_file_path'		=> $this->input->post('league_file_path'),
-					'asset_path'			=> $this->input->post('asset_path'),
-					'asset_url'				=> $this->input->post('asset_url'),
-					
-					'header_img'			=> $this->input->post('header_img'),
-					'twitter_string'		=> $this->input->post('twitter_string'),
-					'tweet_count'			=> $this->input->post('tweet_count'),
-					
-					'sql_path'				=> $this->input->post('sql_path'),
-					'auto_split'			=> $this->input->post('auto_split'),
-					'max_sql_size'			=> $this->input->post('max_sql_size'),
-					'limit_load'			=> $this->input->post('limit_load'),
-					
-				);
+        $this->form_validation->set_rules('league_file_path', lang('dbrd_settings_lgdfile'), 'number|xss_clean');
+        $this->form_validation->set_rules('asset_path', lang('dbrd_settings_assetpath'), 'required|trim|xss_clean');
+        $this->form_validation->set_rules('asset_url', lang('dbrd_settings_asseturl'), 'required|trim|xss_clean');
 
-				if (write_config('league_manager', $data)) {
-					// Success, so reload the page, so they can see their settings
-					Template::set_message('League settings successfully saved.', 'success');
-					redirect(SITE_AREA .'/settings/league_manager');
-				}
-				else
-				{
-					Template::set_message('There was an error saving the file: config/league_manager.', 'error');
-				}
-			}
-		}
+        $this->form_validation->set_rules('header_img', lang('home_settings_header'), 'trim|xss_clean');
+        $this->form_validation->set_rules('twitter_string', lang('home_settings_twitter'), 'trim|xss_clean');
+        $this->form_validation->set_rules('tweet_count', lang('home_settings_tweets'), 'trim|xss_clean');
 
-		// Load our current settings
-		Template::set(read_config('league_manager'));
+        $this->form_validation->set_rules('sql_path', lang('sql_settings_mysqlpath'), 'required|trim|xss_clean');
+        $this->form_validation->set_rules('max_sql_size', lang('sql_settings_max'), 'number|xss_clean');
+        $this->form_validation->set_rules('auto_split', lang('sql_settings_autosplit'), 'number|xss_clean');
+        $this->form_validation->set_rules('limit_load', lang('sql_settings_auto_load'), 'required|number|xss_clean');
 
-		Template::set('toolbar_title', lang('dbrd_settings_title'));
-        Template::set_view('league_manager/settings/index');
-		Template::render();
+        if ($this->form_validation->run() === false)
+        {
+            return false;
+        }
+
+		$data = array(
+            array('name' => 'ootp.game_version', 'value' => $this->input->post('game_version')),
+            array('name' => 'ootp.league_id', 'value' => $this->input->post('league_id')),
+            array('name' => 'ootp.use_ootp_details', 'value' => ($this->input->post('use_ootp_details')) ? 1 : -1),
+            array('name' => 'ootp.league_name', 'value' => $this->input->post('league_name')),
+            array('name' => 'ootp.league_abbr', 'value' => $this->input->post('league_abbr')),
+            array('name' => 'ootp.league_icon', 'value' => $this->input->post('league_icon')),
+            array('name' => 'ootp.league_txtcolor', 'value' => $this->input->post('league_txtcolor')),
+            array('name' => 'ootp.league_bgcolor', 'value' => $this->input->post('league_bgcolor')),
+            array('name' => 'ootp.league_file_path', 'value' => $this->input->post('league_file_path')),
+            array('name' => 'ootp.asset_path', 'value' => $this->input->post('asset_path')),
+            array('name' => 'ootp.asset_url', 'value' => $this->input->post('asset_url')),
+            array('name' => 'ootp.header_img', 'value' => $this->input->post('header_img')),
+            array('name' => 'ootp.twitter_string', 'value' => $this->input->post('twitter_string')),
+            array('name' => 'ootp.tweet_count', 'value' => $this->input->post('tweet_count')),
+            array('name' => 'ootp.sql_path', 'value' => $this->input->post('sql_path')),
+            array('name' => 'ootp.auto_split', 'value' => $this->input->post('auto_split')),
+            array('name' => 'ootp.max_sql_size', 'value' => $this->input->post('max_sql_size')),
+            array('name' => 'ootp.limit_load', 'value' => $this->input->post('limit_load')),
+
+        );
+        //destroy the saved update message in case they changed update preferences.
+        if ($this->cache->get('update_message'))
+        {
+            if (!is_writeable(FCPATH.APPPATH.'cache/'))
+            {
+                $this->cache->delete('update_message');
+            }
+        }
+
+        // Log the activity
+        $this->activity_model->log_activity($this->auth->user_id(), lang('bf_act_settings_saved').': ' . $this->input->ip_address(), 'core');
+
+        // save the settings to the DB
+        $updated = $this->settings_model->update_batch($data, 'name');
+
+        return $updated;
+
 	}
 	
 }
--- views/settings/index.php
+++ views/settings/index.php
@@ -12,13 +12,13 @@
 <legend><?php echo lang('sim_setting_title'); ?></legend>
 </fieldset>
 	<div>
-		<label class="required" for="view_type"><?php echo lang('dbrd_settings_gamever'); ?></label>
-		<select id="ootp_version" name="ootp_version" class="small" >
+		<label class="required"><?php echo lang('dbrd_settings_gamever'); ?></label>
+		<select id="game_version" name="game_version" class="small" >
 		<?php
 			$versions = loadOOTPVersions();
 			foreach( $versions as $ver => $label){
 				echo('<option value="'.$ver.'"');
-				if (isset($ootp_version) && $ootp_version == $value) { 
+				if (isset($settings['ootp.game_version']) && $settings['ootp.game_version'] == $value) { 
 					echo(' selected="selected"');
 				}
 				echo('">'.$label.'</option>');
@@ -31,13 +31,13 @@
 		<!-- LEAGUE ID -->
 	<div>
 		<label class="required" for="league_id"><?php echo lang('dbrd_settings_leagueid'); ?></label>
-		<input type="text" class="tiny" id="league_id" name="league_id" value="<?php echo (isset($league_id)) ? $league_id: set_value('league_id'); ?>" /> <span><?php echo lang('dbrd_settings_leagueid_note'); ?></span>
+		<input type="text" class="tiny" id="league_id" name="league_id" value="<?php echo (isset($settings['ootp.league_id'])) ? $settings['ootp.league_id']: set_value('ootp.league_id'); ?>" /> <span><?php echo lang('dbrd_settings_leagueid_note'); ?></span>
 	</div>
 		<!-- OOTP DETAILS OVERRIDE -->
 	<div>
-		<label for="use_ootp_details"><?php echo lang('dbrd_settings_useootp'); ?></label>
+		<label><?php echo lang('dbrd_settings_useootp'); ?></label>
 		<?php
-		$use_selection = ((isset($use_ootp_details) && $use_ootp_details == 1) || !isset($use_ootp_details)) ? true : false;
+		$use_selection = ((isset($settings['ootp.use_ootp_details']) && $settings['ootp.use_ootp_details'] == 1) || !isset($settings['ootp.use_ootp_details'])) ? true : false;
 		echo form_checkbox('use_ootp_details',1, $use_selection,'id="use_ootp_details"');
 		?>
 	</div>
@@ -45,27 +45,27 @@
             <!-- LEAGUE NAME -->
         <div>
             <label for="league_name"><?php echo lang('dbrd_settings_lgname'); ?></label>
-            <input type="text" id="league_name" name="league_name" value="<?php echo (isset($league_name)) ? $league_name: set_value('league_name'); ?>" />
+            <input type="text" id="league_name" name="league_name" value="<?php echo (isset($settings['ootp.league_name'])) ? $settings['ootp.league_name']: set_value('ootp.league_name'); ?>" />
         </div>
             <!-- LEAGUE ABBR -->
         <div>
             <label for="league_abbr"><?php echo lang('dbrd_settings_lgabbr'); ?></label>
-            <input type="text" class="small" id="league_abbr" name="league_abbr" value="<?php echo (isset($league_abbr)) ? $league_abbr: set_value('league_abbr'); ?>" />
+            <input type="text" class="small" id="league_abbr" name="league_abbr" value="<?php echo (isset($settings['ootp.league_abbr'])) ? $settings['ootp.league_abbr']: set_value('ootp.league_abbr'); ?>" />
         </div>
             <!-- LEAGUE ICON -->
         <div>
             <label for="league_icon"><?php echo lang('dbrd_settings_lgicon'); ?></label>
-            <input type="text" class="small" id="league_icon" name="league_icon" value="<?php echo (isset($league_icon)) ? $league_icon: set_value('league_icon'); ?>" />
+            <input type="text" class="small" id="league_icon" name="league_icon" value="<?php echo (isset($settings['ootp.league_icon'])) ? $settings['ootp.league_icon']: set_value('ootp.league_icon'); ?>" />
         </div>
             <!-- LEAGUE Text Color -->
         <div>
             <label for="league_txtcolor"><?php echo lang('dbrd_settings_txtcolor'); ?></label>
-            <input type="text" class="small" id="league_txtcolor" name="league_txtcolor" value="<?php echo (isset($league_txtcolor)) ? $league_txtcolor: set_value('league_txtcolor'); ?>" />
+            <input type="text" class="small" id="league_txtcolor" name="league_txtcolor" value="<?php echo (isset($settings['ootp.league_txtcolor'])) ? $settings['ootp.league_txtcolor']: set_value('ootp.league_txtcolor'); ?>" />
         </div>
             <!-- LEAGUE BG Color -->
         <div>
             <label for="league_bgcolor"><?php echo lang('dbrd_settings_bgcolor'); ?></label>
-            <input type="text" class="small" id="league_bgcolor" name="league_bgcolor" value="<?php echo (isset($league_bgcolor)) ? $league_bgcolor: set_value('league_bgcolor'); ?>" />
+            <input type="text" class="small" id="league_bgcolor" name="league_bgcolor" value="<?php echo (isset($settings['ootp.league_bgcolor'])) ? $settings['ootp.league_bgcolor']: set_value('ootp.league_bgcolor'); ?>" />
         </div>
     </div>
 <fieldset>
@@ -75,18 +75,18 @@
 	<div>
 		<label for="header_img"><?php echo lang('home_settings_header'); ?></label>
 		<input type="file" id="header_img" name="header_img" /><br />
-		<span class="form_inst">Current Image: <?php echo (isset($header_img)) ? $header_img: set_value('header_img'); ?></span>
+		<span class="form_inst">Current Image: <?php echo (isset($settings['ootp.header_img'])) ? $settings['ootp.header_img']: set_value('ootp.header_img'); ?></span>
 	</div>
 		<!-- Twitter String -->
 	<div>
 		<label for="twitter_string"><?php echo lang('home_settings_twitter'); ?></label>
-		<input type="text" id="twitter_string" name="twitter_string" value="<?php echo (isset($twitter_string)) ? $twitter_string: set_value('twitter_string'); ?>" /><br />
+		<input type="text" id="twitter_string" name="twitter_string" value="<?php echo (isset($settings['ootp.twitter_string'])) ? $settings['ootp.twitter_string']: set_value('ootp.twitter_string'); ?>" /><br />
 		<span class="subcaption"><?php echo lang('dbrd_settings_twtrwnote'); ?></span>
 	</div>
 			<!-- Tweet Count -->
 	<div>
 		<label for="tweet_count"><?php echo lang('home_settings_tweets'); ?></label>
-		<input type="text" class="tiny" id="tweet_count" name="tweet_count" value="<?php echo (isset($tweet_count)) ? $tweet_count: set_value('tweet_count'); ?>" /><br />
+		<input type="text" class="tiny" id="tweet_count" name="tweet_count" value="<?php echo (isset($settings['ootp.tweet_count'])) ? $settings['ootp.tweet_count']: set_value('ootp.tweet_count'); ?>" /><br />
 	</div>
 	
 <fieldset>
@@ -96,20 +96,20 @@
 		<!-- League File Server Path -->
 	<div>
 		<label for="league_file_path"><?php echo lang('dbrd_settings_lgdfile'); ?></label>
-		<input type="text" id="league_file_path" name="league_file_path" value="<?php echo (isset($league_file_path)) ? $league_file_path: set_value('league_file_path'); ?>" /><br />
+		<input type="text" id="league_file_path" name="league_file_path" value="<?php echo (isset($settings['ootp.league_file_path'])) ? $settings['ootp.league_file_path']: set_value('ootp.league_file_path'); ?>" /><br />
 		<span class="subcaption"><?php echo lang('dbrd_settings_lgdfilenote'); ?></span>
 	</div>
 	
 		<!-- Asset Path -->
 	<div>
 		<label class="required" for="asset_path"><?php echo lang('dbrd_settings_assetpath'); ?></label>
-		<input type="text" id="asset_path" name="asset_path" value="<?php echo (isset($asset_path)) ? $asset_path: set_value('asset_path'); ?>" /><br />
+		<input type="text" id="asset_path" name="asset_path" value="<?php echo (isset($settings['ootp.asset_path'])) ? $settings['ootp.asset_path']: set_value('ootp.asset_path'); ?>" /><br />
         <span class="subcaption"><?php echo lang('dbrd_settings_assetpnote'); ?></span>
 	</div>
 		<!-- Asset URL -->
 	<div>
 		<label class="required" for="asset_url"><?php echo lang('dbrd_settings_asseturl'); ?></label>
-		<input type="text" id="asset_url" name="asset_url" value="<?php echo (isset($asset_url)) ? $asset_url: set_value('asset_url'); ?>" /><br />
+		<input type="text" id="asset_url" name="asset_url" value="<?php echo (isset($settings['ootp.asset_url'])) ? $settings['ootp.asset_url']: set_value('ootp.asset_url'); ?>" /><br />
 		<span class="subcaption"><?php echo lang('dbrd_settings_assetunote'); ?></span>
 	</div>
 
@@ -119,21 +119,21 @@
 	<!-- SQL FILE PATH -->
 	<div>
 		<label class="required" for="sql_path"><?php echo lang('sql_settings_mysqlpath'); ?></label>
-		<input type="text" id="sql_path" name="sql_path" value="<?php echo (isset($sql_path)) ? $sql_path: set_value('sql_path'); ?>" /><br />
+		<input type="text" id="sql_path" name="sql_path" value="<?php echo (isset($settings['ootp.sql_path'])) ? $settings['ootp.sql_path']: set_value('ootp.sql_path'); ?>" /><br />
         <span class="subcaption"><?php echo lang('sql_settings_path_note'); ?></span>
 	</div>
 	<!-- MAX SIZE -->
 	<div>
 		<label for="max_sql_size"><?php echo lang('sql_settings_max'); ?></label>
-		<input type="text" class="tiny" id="max_sql_size" name="max_sql_size" value="<?php echo (isset($max_sql_size)) ? $max_sql_size: set_value('max_sql_size'); ?>" />
+		<input type="text" class="tiny" id="max_sql_size" name="max_sql_size" value="<?php echo (isset($settings['ootp.max_sql_size'])) ? $settings['ootp.max_sql_size']: set_value('ootp.max_sql_size'); ?>" />
         <?php echo lang('sql_settings_max_note'); ?>
 	</div>
 	<!-- AUTO SPLIT -->
 	<div>
 		<label for="auto_split"><?php echo lang('sql_settings_autosplit'); ?></label>
         <?php
-        $use_selection = ((isset($auto_split) && $auto_split == 1)) ? true : false;
-        echo form_checkbox('auto_split',1, $use_selection,'id="auto_split"');
+        $use_selection = ((isset($settings['ootp.auto_split']) && $settings['ootp.auto_split'] == 1)) ? true : false;
+        echo form_checkbox('ootp.auto_split',1, $use_selection,'id="auto_split"');
         ?>
         <span><?php echo lang('sql_settings_autosplit_note'); ?></span>
 	</div>
@@ -142,13 +142,14 @@
 	<div>
 		<label for="limit_load"><?php echo lang('sql_settings_loadaction'); ?></label>
 		<?php
-		$limit_selection = (isset($limit_load) && $limit_load == 1) ? true : false;
+		$limit_selection = (isset($settings['ootp.limit_load']) && $settings['ootp.limit_load'] == 1) ? true : false;
 		echo form_radio('limit_load',1, $limit_selection);
 		echo(lang('sql_settings_load_limit')."<br />");
 		?>
 		<label for="limit_load">&nbsp;</label>
 		<?php
-		echo form_radio('limit_load',-1, $limit_selection);
+        $limit_selection = (isset($settings['ootp.limit_load']) && $settings['ootp.limit_load'] == -1) ? true : false;
+        echo form_radio('limit_load',-1, $limit_selection);
 		echo(lang('sql_settings_load_all'));
 		?>
 	</div>
