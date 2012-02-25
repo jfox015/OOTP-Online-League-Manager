187c153598d255e75a820595192884e6475ee467
Jeff Fox <jfox015@gmail.com>
2012-02-25 02:55:25 -0500

    Fixed controller bug

--- controllers/league_manager.php
+++ controllers/league_manager.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class League_Manager extends Admin_Controller {
+class League_Manager extends Front_Controller {
 
 	//--------------------------------------------------------------------
 	
